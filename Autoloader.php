891b2eb9b04f22c72118317fc77714891a490960
igorwulff <igorwulff@gmail.com>
2014-08-06 11:54:23 +0200

    Update Autoloader.php

--- Autoloader.php
+++ Autoloader.php
@@ -1,6 +1,12 @@
 <?php
 namespace Dragonfly;
 
+/**
+ * The Default autoloader used for loading files in the Dragonfly package and other libraries
+ * 
+ * @package     Dragonfly
+ * @subpackage  Framework
+ */
 class Autoloader {
   
   
