194913db1500c981d9bd6fabbcf39c757be1bf36
igorwulff <igorwulff@gmail.com>
2014-08-06 11:47:28 +0200

    Update Datasource.php

--- Datamapper/Datasource.php
+++ Datamapper/Datasource.php
@@ -3,6 +3,19 @@ namespace Dragonfly\Datamapper;
 
 abstract class Datasource {
   
+  protected $handle;
   
+  abstract public function connect(array $data);
   
+  public function disconnect(){
+    $this->handle = null;
+  }
+  
+  public function isConnected(){
+    return !is_null($this->getHandle());
+  }
+  
+  public function getHandle(){
+    return $this->handle;
+  }
 }
