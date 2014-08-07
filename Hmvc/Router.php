18b1bb14ec59a8c5b3f531576e2ec0457e944b59
igorwulff <igorwulff@gmail.com>
2014-08-07 18:49:52 +0200

    Update Router.php

--- Hmvc/Router.php
+++ Hmvc/Router.php
@@ -1,1 +1,29 @@
 <?php
+namespace Dragonfly\Hmvc;
+use Dragonfly\Network\Uri;
+
+class Router {
+  
+	protected $requestUri;
+	protected $routes;
+  
+	public function __construct(Uri $requestUri, array $routes = array()){
+	  $this->requestUri = $requestUri;
+		$this->setRoutes($routes);
+	}
+	
+	public function setRoutes(array $routes){
+		$this->routes = $routes;
+	}
+	
+	public function getRequestUri(){
+		return $this->requestUri;
+	}
+	
+	public function lookup(Uri $uri){
+		
+		
+		
+	}
+  
+}
