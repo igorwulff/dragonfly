5807d0eef3d6803e6f78d7b49a38522e6c23a233
igorwulff <igorwulff@gmail.com>
2014-08-08 09:45:25 +0200

    Update Autoloader.php

--- Autoloader.php
+++ Autoloader.php
@@ -2,13 +2,57 @@
 namespace Dragonfly;
 
 /**
- * The Default autoloader used for loading files in the Dragonfly package and other libraries
+ * The default autoloader used for loading files in the Dragonfly package or other libraries
  * 
  * @package     Dragonfly
  * @subpackage  Framework
  */
 class Autoloader {
   
-  
+	private $namespace;
+	private $pathList = array();
+	private $seperator;
+	private $extensionList = array();
+	
+	public function __construct($namespace, $seperator = "\\", array $extensionList = array('php', 'phtml')){
+	  $this->namespace = $namespace;
+	  $this->seperator = $seperator;
+	  
+    foreach($extensionList as $value){
+      $this->addExtension($value);
+    }
+	}
+	
+	public function addPath($path, $append = true){
+		return (bool)$append === true ? array_push($this->pathList, $path) : array_unshift($this->pathList, $path);
+	}
+	
+	public function addExtension($extension){
+		return array_push($this->extensionList, $extension);
+	}
+	
+	public function register(){
+		spl_autoload_register(array($this, 'load'));
+	}
+	
+	private function load($name){
+		$filename = '';
+		foreach($this->pathList as $path){
+			$filename = $path.$name.$this->extension;
+			if(file_exists($filename)){
+				require_once $filename;
+				return true;
+			}
+		}
+
+		if(!file_exists($filename)){
+			throw new \Exception('File '.$filename.' not found');
+		} else if(!class_exists($name, false)){
+			// Make sure we throw an Exception so we have a stacktrace
+			throw new \Exception('Class "'.$name.'" not found');
+		}
+
+		return;
+	}
   
 }
