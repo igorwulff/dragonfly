ef862ae9439296d85d1257fe0bee3cb6eda92357
igorwulff <igorwulff@gmail.com>
2014-08-12 16:23:40 +0200

    Update Autoloader.php

--- Autoloader.php
+++ Autoloader.php
@@ -13,46 +13,81 @@ class Autoloader {
 	private $seperator;
 	private $extensionList = array();
 	
-	public function __construct(array $pathList = array(), $seperator = "\\", array $extensionList = array('php', 'phtml')){
-		foreach($pathList as $value){
-			$this->addPath($value);
-		}
-		
-		$this->seperator = $seperator;
-		
-		foreach($extensionList as $value){
-			$this->addExtension($value);
-		}
-	}
-	
+	/**
+	 * Adds a path to the path list
+	 * 
+	 * @param string $path
+	 * @param bool $append Whether to append or prepend the path
+	 * 
+	 * @return int Returns the number of paths
+	 */
 	public function addPath($path, $append = true){
 		return (bool)$append === true ? array_push($this->pathList, $path) : array_unshift($this->pathList, $path);
 	}
 	
+	/**
+	 * Removes a path from the path list
+	 * 
+	 * @param string $path
+	 */
 	public function removePath($path){
 		array_key_exists($this->pathList, $path) ? unset($this->pathlist[$path]) : throw new \BadMethodException("Path $path does not exist.");
 	}
 	
+	/**
+	 * Retrieve an array of paths
+	 * 
+	 * @return array Returns a list of paths
+	 */
 	public function getPathList(){
 		return $this->pathlist;
 	}
 	
+	/**
+	 * Adds a extension to the extension list
+	 * 
+	 * @param string $extension
+	 * @param bool $append Whether to append or prepend the extension
+	 * 
+	 * @return int Returns the number of extensions
+	 */
 	public function addExtension($extension, $append = true){
 		return (bool)$append === true ? array_push($this->extensionList, $extension) : array_unshift($this->extensionList, $extension);
 	}
 	
+	/**
+	 * Removes a extension from the extension list
+	 * 
+	 * @param string $extension
+	 */
 	public function removeExtension($extension){
 		array_key_exists($this->extensionList, $extension) ? unset($this->extensionList[$extension]) : throw new \BadMethodException("Extension $extension does not exist.");
 	}
 	
+	/**
+	 * Retrieve an array of extensions
+	 * 
+	 * @return array Returns a list of extensions
+	 */
 	public function getExtensionList(){
 		return $this->extensionList;
 	}
 	
+	/**
+	 * Register the autoloader
+	 * 
+	 * The register method binds the load method to the spl_autoload_register 
+	 * function in order to let files be autoloaded.
+	 */
 	public function register(){
 		spl_autoload_register(array($this, 'load'));
 	}
 	
+	/**
+	 * Load the requested file
+	 * 
+	 * @return bool Returns whether a file has been found.
+	 */
 	private function load($name){
 		$filename = '';
 		
