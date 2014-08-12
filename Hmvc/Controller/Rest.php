8a917a207d0f7865da4fff789a37e92e01277c60
igorwulff <igorwulff@gmail.com>
2014-08-12 20:39:05 +0200

    Update Rest.php

--- Hmvc/Controller/Rest.php
+++ Hmvc/Controller/Rest.php
@@ -3,4 +3,15 @@ namespace Dragonfly\Hmvc\Controller;
 
 abstract class Rest {
 
+  public function postAction(){
+    
+  }
+
+  public function getAction(){
+    
+  }
+  
+  public function deleteAction(){
+    
+  }
 }
