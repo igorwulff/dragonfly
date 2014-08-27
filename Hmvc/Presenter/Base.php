c88ea3211644592d92e39fcc684029495c04256e
igorwulff <igorwulff@gmail.com>
2014-08-27 18:22:49 +0200

    Create Base.php

---
+++ Hmvc/Presenter/Base.php
@@ -0,0 +1,22 @@
+<?php
+namespace Dragonfly\Hmvc\Presenter;
+
+class Base {
+  
+  public function url($url){
+  
+  }
+  
+  public function clean($string)(
+  
+  }
+  
+  public function resource($resource){
+    
+  }
+  
+  public function text($string){
+    
+  }
+  
+}
