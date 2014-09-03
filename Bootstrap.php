653f35e24edc825099d88bb0c5d2c4e058a0218b
igorwulff <igorwulff@gmail.com>
2014-09-03 09:36:46 +0200

    Update Bootstrap.php

--- Bootstrap.php
+++ Bootstrap.php
@@ -4,13 +4,15 @@ namespace Dragonfly;
 abstract class Bootstrap {
 
   public function __construct(){
+    $this->setAutoloader();
+  }
+  
+  public function run(){
     
   }
   
   protected function setAutoloader(){
-  
+    
   }
-  
-  
 
 }
