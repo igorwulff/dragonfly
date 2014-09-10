81e6a3f5fec2fd7feaa226d4600edb7a774b69f5
igorwulff <igorwulff@gmail.com>
2014-09-10 14:39:21 +0200

    Update Front.php

--- Hmvc/Controller/Front.php
+++ Hmvc/Controller/Front.php
@@ -3,6 +3,11 @@ namespace Dragonfly\Hmvc\Controller;
 
 abstract class Front extends Base {
   
+  public function __construct(Request $request){
+    parent::__construct($request);
+    $this->prepareLayout();
+  }
+  
   public function prepareLayout(){
     
   }
