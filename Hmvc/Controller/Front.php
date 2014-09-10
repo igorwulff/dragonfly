be61029d656b6af825d7722827c694df8e6ee63c
igorwulff <igorwulff@gmail.com>
2014-09-10 14:52:47 +0200

    Update Front.php

--- Hmvc/Controller/Front.php
+++ Hmvc/Controller/Front.php
@@ -3,16 +3,29 @@ namespace Dragonfly\Hmvc\Controller;
 
 abstract class Front extends Base {
   
+  protected $controllerStorage;
+  
   public function __construct(Request $request){
     parent::__construct($request);
+    $this->controllerStorage = new \SplObjectStorage();
     $this->prepareLayout();
   }
   
-  protected function prepareLayout(){
+  protected abstract function prepareLayout();
     
+  protected function attachController(Controller $controller){
+    $this->controllerStorage->attach($controller);
+  }
+  
+  protected function detachController(Controller $controller){
+    $this->controllerStorage->detach($controller);
+  }
+  
+  protected function getControllerStorage(){
+    return $this->controllerStorage;
   }
   
   public function output(){
-    
+
   }
 }
