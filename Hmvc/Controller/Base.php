74d350b2678853c0de3c13a55f140dff6782bd03
igorwulff <igorwulff@gmail.com>
2014-09-10 15:02:12 +0200

    Update Base.php

--- Hmvc/Controller/Base.php
+++ Hmvc/Controller/Base.php
@@ -5,9 +5,19 @@ use \Dragonfly\Hmvc\Request;
 abstract class Base {
 
   protected $request;
+  protected $model;
+  protected $view;
 
   public function __construct(Request $request){
     $this->request = $request;
+  }
+  
+  protected function setModel(Model $model){
+    
+  }
+  
+  protected function setView($view){
+    
   }
   
   public function getRequest(){
