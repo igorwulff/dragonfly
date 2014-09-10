c780bbdbcc928fb3ffe00008eec35b1e390a5614
igorwulff <igorwulff@gmail.com>
2014-09-10 14:44:32 +0200

    Update Base.php

--- Hmvc/Controller/Base.php
+++ Hmvc/Controller/Base.php
@@ -5,14 +5,29 @@ use \Dragonfly\Hmvc\Request;
 abstract class Base {
 
   protected $request;
+  protected $controllerStorage;
 
   public function __construct(Request $request){
     $this->request = $request;
+    $this->controllerStorage = new \SplObjectStorage();
   }
   
   public function getRequest(){
     return $this->request;
   }
+  
+  protected function attachChild(Controller $controller){
+    $this->controllerStorage->attach($controller);
+  }
+  
+  protected function detachChild(Controller $controller){
+    $this->controllerStorage->detach($controller);
+  }
+  
+  protected function getControllerStorage(){
+    return $this->controllerStorage;
+  }
 
   public abstract function output();
+  
 }
