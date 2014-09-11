2e797154a5c6d46390580ce2ab5ba106505f1394
igorwulff <igorwulff@gmail.com>
2014-09-11 09:50:49 +0200

    Update Base.php

--- Hmvc/Controller/Base.php
+++ Hmvc/Controller/Base.php
@@ -1,28 +1,36 @@
 <?php
 namespace Dragonfly\Hmvc\Controller;
 use \Dragonfly\Hmvc\Request;
+use \Dragonfly\Hmvc\Presenter\Base as Presenter;
 
 abstract class Base {
 
   protected $request;
-  protected $model;
-  protected $view;
 
   public function __construct(Request $request){
     $this->request = $request;
   }
   
-  protected function setModel(Model $model){
+  protected function displayView($view, $presenter = null){
+    if(is_null($presenter)){
+      $presenter = new Presenter($this, $view);
+    } else {
+      $presenter = new $presenter($this, $view);
+      if(!$presenter instanceof Presenter){
+        throw new \BadMethodException("Invalid presenter given: ".get_class($presenter));
+      }
+    }
     
-  }
-  
-  protected function setView($view){
     
   }
   
   public function getRequest(){
     return $this->request;
   }
+  
+  public abstract function actionNotFound();
+  
+  public abstract function actionAccessDenied();
 
   public abstract function output();
   
